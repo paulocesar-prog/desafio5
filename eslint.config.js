ebed490388b1ee8cbc7bbba7ba6b674d2c783c76
Paulo Fernandes <paulo_fernandes@outlook.com>
2025-10-23 17:03:15 -0300

    configuração workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,30 @@
-module.exports = {
-    env: {
-        node: true,
-        es2022: true,
-        jest: true
-    },
-    extends: [
-        'eslint:recommended'
-    ],
-    parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module'
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
     },
     rules: {
-        'no-console': 'off',
-        'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-        'semi': ['error', 'always'],
-        'quotes': ['error', 'single'],
-        'indent': ['error', 2]
-    },
-    ignorePatterns: [
-        'node_modules/',
-        'coverage/',
-        '*.min.js'
+      'no-console': 'off',
+      'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2]
+    }
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'coverage/',
+      '*.min.js'
     ]
-};
+  }
+];
